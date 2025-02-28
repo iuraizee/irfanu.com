6251c5b9b0b5a1b08732e3fecd7c2da8acb6f18f
Irfan <iuraizee@gmail.com>
2025-02-27 22:11:33 -0600

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  trailingSlash: true,
+  reactStrictMode: false,
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
