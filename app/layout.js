ddd8e3f517b4172919c6b341d278c31cdeece4a3
iuraizee <iuraizee@gmail.com>
2025-03-05 21:52:21 -0600

    feat: add google analytics

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,14 @@
 import 'modern-normalize/modern-normalize.css'
 import '../styles/base.scss'
+import Script from 'next/script'
 
 export const metadata = {
   metadataBase: new URL('https://irfanu.com'),
   title: {
     default: 'Irfan Uraizee: Designer and developer',
   },
-  description: 'Combining design and programming to build interactive experiences',
+  description:
+    'Combining design and programming to build interactive experiences',
   authors: [{ name: 'Irfan Uraizee' }],
   icons: {
     icon: '/favicon.ico',
@@ -24,6 +26,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-JBH6RVSDVW" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-JBH6RVSDVW');
+        `}
+      </Script>
     </html>
   )
 }
