ae04424a79d319e58c5837c8e2710d6f74e6dc8a
iuraizee <iuraizee@gmail.com>
2025-03-05 21:17:33 -0600

    feat: add page metadata

--- app/page.js
+++ app/page.js
@@ -5,41 +5,34 @@ import SocialLinks from '../components/SocialLinks.js'
 
 export default function Home() {
   return (
-    <div>
-      {/* <Head> */}
-      {/* <title>Irfan Uraizee</title> */}
-      {/* <link rel="canonical" href="https://irfanu.com/" /> */}
-      {/* </Head> */}
+    <main>
+      <header>
+        <h1 style={{ display: 'none' }}>Irfan Uraizee</h1>
 
-      <main>
-        <header>
-          <h1 style={{ display: 'none' }}>Irfan Uraizee</h1>
+        {data.intro.map((element, i) => {
+          return (
+            <p key={i} dangerouslySetInnerHTML={{ __html: element.value }} />
+          )
+        })}
+        <SocialLinks />
+      </header>
 
-          {data.intro.map((element, i) => {
+      <section>
+        <h2>Selected projects</h2>
+
+        <div className="card-grid">
+          {projects.map((el, i) => {
             return (
-              <p key={i} dangerouslySetInnerHTML={{ __html: element.value }} />
+              <Card
+                key={i}
+                url={el.url}
+                headline={el.headline}
+                image={el.image}
+              />
             )
           })}
-          <SocialLinks /> 
-        </header>
-
-        <section>
-          <h2>Selected projects</h2>
-
-          <div className="card-grid">
-            {projects.map((el, i) => {
-              return (
-                <Card
-                  key={i}
-                  url={el.url}
-                  headline={el.headline}
-                  image={el.image}
-                />
-              )
-            })}
-          </div>
-        </section>
-      </main>
-    </div>
+        </div>
+      </section>
+    </main>
   )
 }
