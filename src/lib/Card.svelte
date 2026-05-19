e4e90a3976ed2009c8fd5e1ea9c47d755ecc05ee
Irfan Uraizee <iuraizee@gmail.com>
2026-05-19 00:25:55 -0400

    feat: add blog post cta

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,7 +1,7 @@
 <script>
-  import { Loader2 } from 'lucide-svelte'
+  import { Loader2, ArrowRight } from 'lucide-svelte'
 
-  let { url, headline, image } = $props()
+  let { url, headline, image, blogUrl = null } = $props()
   let isLoaded = $state(false)
 </script>
 
@@ -22,4 +22,9 @@
       <p class="card-text">{@html headline}</p>
     </div>
   </a>
+  {#if blogUrl}
+    <div class="card-blog">
+      <a href={blogUrl}>About this project <ArrowRight class="arrow-right" /></a>
+    </div>
+  {/if}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -23,7 +23,7 @@
 
     <div class="card-grid">
       {#each projects as project}
-        <Card url={project.url} headline={project.headline} image={project.image} />
+        <Card url={project.url} headline={project.headline} image={project.image} blogUrl={project.blogUrl} />
       {/each}
     </div>
   </section>
